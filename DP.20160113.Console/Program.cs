c7d4a494185065e3a8d44d7681b4ac77a608ebf5
daroczi.krisztian <daroczi.krisztian@gmail.com>
2016-01-16 12:49:47 +0200

    DP 2016 01 13 initial commit.

---
+++ DP.20160113.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP._20160113.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
