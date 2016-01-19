0991460f5d4c9b43ce4e8ae2d6e130e9c27f779e
daroczi.krisztian <daroczi.krisztian@gmail.com>
2016-01-19 14:00:35 +0200

    Throw exception for incorrect input from the char distance calculator.

--- DP.20160113.BLL/Strings/CharDistanceCalculator.cs
+++ DP.20160113.BLL/Strings/CharDistanceCalculator.cs
@@ -10,11 +10,12 @@ namespace DP._20160113.BLL.Strings
 		/// <param name="s">The first string.</param>
 		/// <param name="t">The second string.</param>
 		/// <returns>The distance</returns>
+		/// <exception cref="ArgumentException">If the strings don't have the same length</exception>
 		public int GetDistance(string s, string t)
 		{
 			// only same length strings can be used
 			if (s.Length != t.Length)
-				return -1;
+				throw new ArgumentException("Only same length-strings are allowed for char distance calculation.");
 
 			int distance = 0;
 			char[] left = s.ToCharArray();
