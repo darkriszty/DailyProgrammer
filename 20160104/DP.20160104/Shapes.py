d155fe1a12b5128e6bc10341167de85e3807b4e8
daroczi.krisztian <daroczi.krisztian@gmail.com>
2016-01-24 13:22:38 +0200

    Calculate the coordinates of the rectangle when the top left or bottom right are set.

--- 20160104/DP.20160104/Shapes.py
+++ 20160104/DP.20160104/Shapes.py
@@ -46,25 +46,25 @@ class Line(Shape):
 class Rect(Shape):
     """represents a rectangle"""
 
-    _topLeft = Point2D()
-    _bottomRight = Point2D()
-
+    # from top left clockwise to bottom left
     _p1 = Point2D()
     _p2 = Point2D()
     _p3 = Point2D()
     _p4 = Point2D()
 
     def setTopLeft(self, pos):
-        self._topLeft = pos
+        self._p1 = pos
         self._updatePoints()
 
     def setBottomRight(self, pos):
-        self._bottomRight = pos
+        self._p3 = pos
         self._updatePoints()
 
     def _updatePoints(self):
-        #TODO
-        pass
+       self._p2.x = self._p1.x;
+       self._p2.y = self._p3.y;
+       self._p4.x = self._p3.x;
+       self._p4.y = self._p1.y;
 
     def draw(self):
         print("drawing rectangle of color {0} to {1} -> {2} -> {3} -> {4}".format(self.color.toString(), self._p1.toString(), self._p2.toString(), self._p3.toString(), self._p4.toString()))
