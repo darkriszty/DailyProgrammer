622e6952ed49434c61353e605ed9d6843c8454be
daroczi.krisztian <daroczi.krisztian@gmail.com>
2016-02-12 11:24:36 +0200

    Initial commit of empty solution.

---
+++ DP.20160210/DP.20160210.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP._20160210.Console
+{
+	class Program
+	{
+		/*
+		* https://www.reddit.com/r/dailyprogrammer/comments/452omr/20160210_challenge_253_intermediate_countdown/
+		*/
+		static void Main(string[] args)
+		{
+		}
+	}
+}
