96ec476ef791e0000afd8ad101f776f21c29e24a
daroczi.krisztian <daroczi.krisztian@gmail.com>
2016-02-12 12:26:37 +0200

    Add tests for GetOperationType.

--- DP.20160210/DP.20160210.Tests/RPN/TokenInfoProviderTests.cs
+++ DP.20160210/DP.20160210.Tests/RPN/TokenInfoProviderTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DP._20160210.BLL.IoC;
+using DP._20160210.BLL.RPN;
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DP._20160210.Tests.RPN
 {
@@ -12,9 +15,93 @@ namespace DP._20160210.Tests.RPN
 		}
 
 		[TestMethod]
-		public void Test_GetOperationType()
+		public void Test_GetOperationType_Addition()
 		{
-			//TODO
+			// arrange
+			var container = new UnityContainer();
+			container.RegisterServices();
+			var tokenInfoProvider = container.Resolve<ITokenInfoProvider>();
+
+			// act
+			OperationType operationType = tokenInfoProvider.GetOperationType('+');
+
+			// assert
+			Assert.AreEqual(operationType, OperationType.Addition);
+		}
+
+		[TestMethod]
+		public void Test_GetOperationType_Subtraction()
+		{
+			// arrange
+			var container = new UnityContainer();
+			container.RegisterServices();
+			var tokenInfoProvider = container.Resolve<ITokenInfoProvider>();
+
+			// act
+			OperationType operationType = tokenInfoProvider.GetOperationType('-');
+
+			// assert
+			Assert.AreEqual(operationType, OperationType.Subtraction);
+		}
+
+		[TestMethod]
+		public void Test_GetOperationType_Multiplication()
+		{
+			// arrange
+			var container = new UnityContainer();
+			container.RegisterServices();
+			var tokenInfoProvider = container.Resolve<ITokenInfoProvider>();
+
+			// act
+			OperationType operationType = tokenInfoProvider.GetOperationType('*');
+
+			// assert
+			Assert.AreEqual(operationType, OperationType.Multiplication);
+		}
+
+		[TestMethod]
+		public void Test_GetOperationType_Division()
+		{
+			// arrange
+			var container = new UnityContainer();
+			container.RegisterServices();
+			var tokenInfoProvider = container.Resolve<ITokenInfoProvider>();
+
+			// act
+			OperationType operationType = tokenInfoProvider.GetOperationType('/');
+
+			// assert
+			Assert.AreEqual(operationType, OperationType.Division);
+		}
+
+		[TestMethod]
+		public void Test_GetOperationType_Unkown1()
+		{
+			// arrange
+			var container = new UnityContainer();
+			container.RegisterServices();
+			var tokenInfoProvider = container.Resolve<ITokenInfoProvider>();
+
+			// act
+			OperationType operationType = tokenInfoProvider.GetOperationType('x');
+
+			// assert
+			Assert.AreEqual(operationType, OperationType.Unkown);
+		}
+
+		[TestMethod]
+		public void Test_GetOperationType_Unkown2()
+		{
+			// arrange
+			var container = new UnityContainer();
+			container.RegisterServices();
+			var tokenInfoProvider = container.Resolve<ITokenInfoProvider>();
+
+			// act
+			OperationType operationType = tokenInfoProvider.GetOperationType('8');
+
+			// assert
+			Assert.AreEqual(operationType, OperationType.Unkown);
 		}
 	}
 }
